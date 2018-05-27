1b83cbc8a92c2ad80b824cfee6fddd364f84ad98
guyskk <guyskk@qq.com>
2018-05-27 20:54:07 +0800

    add test_kernel

---
+++ tests/test_kernel.py
@@ -0,0 +1,34 @@
+import pytest
+import newio.api as nio
+
+from .helper import run_it
+
+
+def test_shutdown():
+    async def main():
+        for i in range(4):
+            await nio.spawn(subtask(i))
+        await nio.sleep(1)
+
+    async def subtask(n):
+        if n == 3:
+            raise KeyboardInterrupt()
+        await nio.sleep(1)
+
+    with pytest.raises(KeyboardInterrupt):
+        nio.run(main())
+
+
+@run_it
+async def test_cancel_dead_task():
+    async def dead_task():
+        while True:
+            try:
+                await nio.sleep(1)
+            except BaseException:
+                pass
+    task = await nio.spawn(dead_task())
+    await task.cancel()
+    await task.join()
+    assert not task.is_alive
+    assert task.error is not None
