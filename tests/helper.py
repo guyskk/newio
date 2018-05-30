7a20a0d0482f470ca9085a4208b4b953f66b33cd
guyskk <guyskk@qq.com>
2018-05-30 23:23:44 +0800

    test queue

---
+++ tests/helper.py
@@ -0,0 +1,11 @@
+import functools
+from newio_kernel import run
+
+
+def run_it(coro_func):
+
+    @functools.wraps(coro_func)
+    def sync_func(*args, **kwargs):
+        return run(coro_func(*args, **kwargs))
+
+    return sync_func
---
+++ tests/test_queue.py
@@ -0,0 +1,35 @@
+from newio import spawn
+from newio.queue import Queue
+from .helper import run_it
+
+
+@run_it
+async def test_queue():
+    queue = Queue(maxsize=2)
+
+    async def producer():
+        for i in range(100):
+            await queue.put(i)
+
+    async def consumer():
+        count = 0
+        while True:
+            i = await queue.get()
+            if i is None:
+                break
+            count += 1
+        return count
+
+    p1 = await spawn(producer())
+    p2 = await spawn(producer())
+    c1 = await spawn(consumer())
+    c2 = await spawn(consumer())
+    c3 = await spawn(consumer())
+    await p1.join()
+    await p2.join()
+    for i in range(3):
+        await queue.put(None)
+    await c1.join()
+    await c2.join()
+    await c3.join()
+    assert sum([c1.result, c2.result, c3.result]) == 200
