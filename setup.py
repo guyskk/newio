b9655628d27f31498d6b99fdda68ec81cfd0a9f3
guyskk <guyskk@qq.com>
2018-08-02 16:34:44 +0800

    Bump version: 0.6.1 → 0.6.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='newio',
-    version='0.6.1',
+    version='0.6.2',
     description='Newio',
     url='https://github.com/guyskk/newio',
     author='guyskk',
